48e639e766963b600c4b981c9008ea2fca36e0b9
Pierre Pudlo <37126309+pierrepudlo@users.noreply.github.com>
2024-04-30 20:57:25 +0200

    New help pages

--- R/nonBayesian_based.R
+++ R/nonBayesian_based.R
@@ -1,50 +1,70 @@
-#' Compute the average-based scorings
+#' Compute the average, median and most likely-based scorings
 #'
-#' @export
+#' @title Non-Bayesian scorings
 #' @param ni A numeric vector of the total number of replicates for each individual
 #' @param si A numeric vector of the number of replicates equal to 1 for each individual
-#' @return A numeric vector of the average-based scorings
+#' @param theta The probability of that T=1, i.e., the prevalence
+#' @param p The false positive rate
+#' @param q The false negative rate
+#' @return A numeric vector of the scorings
+#'
+#' @examples
+#' data("periodontal")
+#' Y_A <- average_based_scorings(periodontal$ni, periodontal$si)
+#' Y_M <- median_based_scorings(periodontal$ni, periodontal$si)
+#' theta <- mean(periodontal$ti)
+#' cat("The prevalence in the data is ", theta, "\n")
+#' p <- mean(Y_A[periodontal$ti == 0])
+#' q <- mean(1-Y_A[periodontal$ti == 1])
+#' Y_L <- most_likely_based_scorings(periodontal$ni, periodontal$si, theta, p, q)
+#'
+#' @name non_bayesian_scorings
+#'
+#' @rdname non_bayesian_scorings
+#' @export
 average_based_scorings <- function(ni, si) {
   si / ni
 }
 
-#' Compute the median-based scorings
-#'
+#' @rdname non_bayesian_scorings
 #' @export
-#' @param ni A numeric vector of the total number of replicates for each individual
-#' @param si A numeric vector of the number of replicates equal to 1 for each individual
-#' @return A numeric vector of the median-based scorings
 median_based_scorings <- function(ni, si) {
   ifelse(si == ni/2, .5, ifelse(si > ni/2, 1, 0))
 }
 
-#' Compute the most likely-based scorings
+
+#' @rdname non_bayesian_scorings
 #' @export
-#' @param ni A numeric vector of the total number of replicates for each individual
-#' @param si A numeric vector of the number of replicates equal to 1 for each individual
-#' @param theta The probability of that T=1
-#' @param p The false positive rate
-#' @param q The false negative rate
-#' @return A numeric vector of the most likely-based scorings
 most_likely_based_scorings <- function(ni, si, theta, p, q) {
   theta*dbinom(si, ni, 1-q) /
     (theta*dbinom(si, ni, 1-q) + (1-theta)*dbinom(si, ni, p))
 }
 
 #' Classification based on thresholding the scorings
-#' @export
+#'
 #' @param scorings A numeric vector of the scorings
 #' @param vL The lower threshold
 #' @param vU The upper threshold
 #' @return A numeric vector of the classification (where .5 = inconclusive)
+#' @export
 classification_from_scoring <- function(scorings, vL, vU) {
   ifelse(scorings < vL, 0, ifelse(scorings > vU, 1, .5))
 }
 
 #' Compute the average,median-based prevalence estimate based on the scorings
-#' @export
+#'
 #' @param scorings A numeric vector of the scorings
 #' @return A numeric value of the prevalence estimator
+#'
+#' @examples
+#' data("periodontal")
+#' theta <- mean(periodontal$ti)
+#' Y_A <- average_based_scorings(periodontal$ni, periodontal$si)
+#' hat_theta_A <- prevalence_estimate(Y_A)
+#' cat("The average-based prevalence estimate is ", hat_theta_A, "\n")
+#' cat("The prevalence in the dataset is ", theta, "\n")
+#'
+#' @export
 prevalence_estimate <- function(scorings) {
   mean(scorings)
 }
--- R/output_fit.R
+++ R/output_fit.R
@@ -1,9 +1,36 @@
-#' Get credible intervals for the fixed parameters of the Bayesian model
+#' Get credible intervals for the fixed parameters of the Bayesian model,
+#' the Bayesian scorings, and the Bayesian prevalence estimate.
 #'
-#' @export
-#' @param fit The stanfit object return by BayesianFit.
+#' @name bayesian_computations
+#' @title Bayesian computations
+#' @param fit The stanfit object return by BayesianFit
 #' @param alpha Error level of the credible intervals
-#' @return A data frame with the credible intervals
+#' @return
+#' The `credint` function returns the credible interval bounds for the fixed
+#' parameters of the Bayesian model. The default error level is 0.05.
+#'
+#' The `bayesian_scoring` function returns the vector of Bayesian scorings.
+#' These scorings are the posterior probabilities
+#' that the true latent T[i] is equal to 1.
+#'
+#' The `bayesian_prevalence_estimate` function returns the posterior mean of the
+#' posterior distribution on the prevalence of T = 1.
+#'
+#' @seealso \code{\link{classification_from_scoring}}
+#'
+#' @examples
+#' data("periodontal")
+#' theta <- mean(periodontal$ti)
+#' fit <- BayesianFit(periodontal$ni, periodontal$si, chains = 2, iter = 5000)
+#' credint(fit)
+#' Y_B <- bayesian_scorings(fit)
+#' T_B <- classification_from_scoring(Y_B, .4, .6)
+#' theta_B <- bayesian_prevalence_estimate(fit)
+#' cat("The Bayesian prevalence estimate is ", theta_B, "\n")
+#' cat("The prevalence in the data is ", theta, "\n")
+#'
+#' @rdname bayesian_computations
+#' @export
 credint <- function(fit, alpha = .05) {
   # Extract the fixed parameters
   pars <- rstan::extract(fit, pars = c("p", "q", "theta"))
@@ -16,11 +43,9 @@ credint <- function(fit, alpha = .05) {
   return(out)
 }
 
-#' Get the posterior probability that T=1 from the Bayesian model
-#'
+
+#' @rdname bayesian_computations
 #' @export
-#' @param fit The stanfit object return by BayesianFit.
-#' @return A vector of posterior probabilities that T=1
 bayesian_scorings <- function(fit){
   # Extract the latent variable
   Ti <- rstan::extract(fit, pars = "Ti")$Ti
@@ -29,10 +54,8 @@ bayesian_scorings <- function(fit){
   return(out)
 }
 
-#' Compute the Bayesian prevalence estimate based on the scorings
+#' @rdname bayesian_computations
 #' @export
-#' @param fit The stanfit object return by BayesianFit.
-#' @return A numeric value of the Bayesian prevalence estimator
 bayesian_prevalence_estimate <- function(fit){
   # Extract the latent variable
   theta <- rstan::extract(fit, pars = "theta")$theta
