17cfed32becf9eb28f75457e77bdbceb1782c4e1
Pierre Pudlo <pierre.pudlo@univ-amu.fr>
2026-01-19 23:53:23 +0100

    New PubMed URL in periodontal help page

--- R/periodontal-data.R
+++ R/periodontal-data.R
@@ -22,7 +22,7 @@
 #' 1990 Jul;25(4):193-6. doi: 10.1111/j.1600-0765.1990.tb00903.x.
 #' Erratum in: J Periodontal Res 1990 Nov;25(6):377. PMID: 2197400.
 #' Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/2197400}{PubMed})
+#' (\href{https://pubmed.ncbi.nlm.nih.gov/2197400/}{PubMed})
 #'
 #'
 #' @examples
